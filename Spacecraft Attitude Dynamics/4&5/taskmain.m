8f7fe502325e19673730c721627fc396795e7efc
sp-droid <52839915+sp-droid@users.noreply.github.com>
2022-11-02 20:37:23 +0100

    DCM

--- Spacecraft Attitude Dynamics/4&5/taskmain.m
+++ Spacecraft Attitude Dynamics/4&5/taskmain.m
@@ -20,15 +20,18 @@ Mr = 0;
 I = diag(I);
 I_inv = pinv(I);
 
-simulation = sim("task.slx");
-w = simulation.w;
-w_d = simulation.w_d;
-time = simulation.time;
+% Initial DCM & quaternion
+A0=[1 0 0; 0 1 0; 0 0 1];
+
+simu = sim("task.slx");
+w = simu.w;
+w_d = simu.w_d;
+time = simu.time;
 
 % Calculate h and T
-I = diag(I);
-T = 0.5*(I(1)*w(1,:).^2+I(2)*w(2,:).^2+I(3)*w(3,:).^2);
-h = sqrt((I(1)*w(1,:)).^2+(I(2)*w(2,:)).^2+(I(3)*w(3,:)).^2);
+Idiag = diag(I);
+T = 0.5*(Idiag(1)*w(1,:).^2+Idiag(2)*w(2,:).^2+Idiag(3)*w(3,:).^2);
+h = sqrt((Idiag(1)*w(1,:)).^2+(Idiag(2)*w(2,:)).^2+(Idiag(3)*w(3,:)).^2);
 
 %% Plots
 figure()
@@ -55,13 +58,13 @@ title('Angular momentum')
 figure()
 % Energy ellipsoid
 % wx^2*Ix/2/T + wy^2*Iy/2/T + wz^2*Iz/2/T = 1
-[X,Y,Z] = ellipsoid(0,0,0,sqrt(2*T(1)/I(1)),sqrt(2*T(1)/I(2)),sqrt(2*T(1)/I(3)));
+[X,Y,Z] = ellipsoid(0,0,0,sqrt(2*T(1)/Idiag(1)),sqrt(2*T(1)/Idiag(2)),sqrt(2*T(1)/Idiag(3)));
 energyEllipsoid = surf(X,Y,Z,'FaceAlpha', 1, 'EdgeColor', 'none');
 hold on
 
 % Ang. momentum ellipsoid
 % wx^2/h^2*Ix^2 + wy^2/h^2*Iy^2 + wz^2/h^2*Iz^2 = 1
-[X,Y,Z] = ellipsoid(0,0,0,h(1)/I(1),h(1)/I(2),h(1)/I(3));
+[X,Y,Z] = ellipsoid(0,0,0,h(1)/Idiag(1),h(1)/Idiag(2),h(1)/Idiag(3));
 momentEllipsoid = surf(X,Y,Z,'FaceAlpha', 0.5, 'EdgeColor', 'none');
 hold on
 
